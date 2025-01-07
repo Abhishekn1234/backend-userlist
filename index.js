3ee1662470492786e4e88ef7afc2104cf6cf02aa
Abhishekn1234 <abhishekpes123@gmail.com>
2025-01-07 18:52:15 +0530

    first-commit

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ mongoose.connect('mongodb://localhost:27017/your_db_name', {
 
 
 app.use('/user', authRoutes);
-
+app.get('/',async(req,res)=>{
+  res.send("Hello from userlist")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
